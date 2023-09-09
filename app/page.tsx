07d438dbcbc10215d271211c8b70599ad1ba9113
jorlar <jqern.larsen@gmail.com>
2023-09-09 18:14:10 +0200

    :sparkles: Edit CSS and create components and layouts

--- app/page.tsx
+++ app/page.tsx
@@ -1,41 +1,15 @@
-/* eslint-disable react/jsx-key */
-import fs from "fs";
-import Link from "next/link";
-import matter from "gray-matter";
-import { PostMetadata } from "@/components/PostMetadata";
-
-const getPostMetadata = (): PostMetadata[] => {
-  const folder = "posts/";
-  const files = fs.readdirSync(folder);
-  const markdownPosts = files.filter((file) => file.endsWith(".md"));
-
-  // Get Gray-matter data from each file
-  const posts = markdownPosts.map((fileName) => {
-    const fileContents = fs.readFileSync(`posts/${fileName}`, "utf8");
-    const matterResult = matter(fileContents);
-    return {
-      title: matterResult.data.title,
-      date: matterResult.data.date,
-      subtitle: matterResult.data.subtitle,
-      slug: fileName.replace(".md", ""),
-    };
-  });
-
-  return posts;
-};
+import getPostMetadata from "../components/getPostMetadata";
+import PostPreview from "../components/PostPreview";
 
 const HomePage = () => {
   const postMetadata = getPostMetadata();
   const postPreviews = postMetadata.map((post) => (
-    <div>
-      <Link href={`/posts/${post.slug}`}>
-        <h2>{post.title}</h2>
-      </Link>
-      <p>{post.subtitle}</p>
-      <p>{post.date}</p>
-    </div>
+    <PostPreview key={post.slug} {...post} />
   ));
-  return <div>{postPreviews}</div>;
+
+  return (
+    <div className='grid grid-cols-1 md:grid-cols-2 gap-4'>{postPreviews}</div>
+  );
 };
 
 export default HomePage;
