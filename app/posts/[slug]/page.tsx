a4ed97ea85de0b2ead79b3aaebdc97ba662f518c
jorlar <jqern.larsen@gmail.com>
2023-09-09 17:10:47 +0200

    :construction: Add Markdown to project

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -1,8 +1,20 @@
-const PostPage = () => {
+import fs from "fs";
+import Markdown from "markdown-to-jsx";
+
+const getPostContent = (slug: string) => {
+  const folder = "posts/";
+  const file = `${folder}${slug}.md`;
+  const content = fs.readFileSync(file, "utf8");
+  return content;
+};
+
+const PostPage = (props: any) => {
+  const slug = props.params.slug;
+  const content = getPostContent(slug);
   return (
-    <p>
-      <h1>This is a post!!</h1>
-    </p>
+    <div>
+      <Markdown>{content}</Markdown>
+    </div>
   );
 };
 
